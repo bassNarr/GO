edd0531927cd4f14fc239a98fa7f3a67f1851f68
K1konei <122950988+K1konei@users.noreply.github.com>
2023-01-19 16:14:33 +0700

    lesson1.1
    data type

---
+++ lesson1.1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Yep Gentleman")
+	/*const a = 7
+	fmt.Println(a)
+	const name string = "Kwan"
+	const age int = 25
+	const score float32 = 99
+	const isPass bool = true
+	fmt.Println("My name's", name)
+	fmt.Println("age ", age)
+	fmt.Println("คะแนน ", score)
+	fmt.Println("Is pass ", isPass)*/
+
+}
